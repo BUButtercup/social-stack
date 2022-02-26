3301c470cc96aa49e11ad8fe9ca356b811f45cc0
Buttercup <andyandnile@gmail.com>
2022-02-25 17:04:14 -0800

    finished all routes, added validations

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { User, Thought } = require('../../models');
 
-
+//get all users
 router.get('/', (req, res)=>{
     User.find({}, (err, result) =>{
         if(err){
@@ -11,11 +11,14 @@ router.get('/', (req, res)=>{
             res.status(200).json(result)
             
         } 
-    }).populate('friends')
-    .populate('thoughts')
-    .select('-__v');
+    })
+    //to turn on ability to see all users friends and thoughts when get all users
+    // .populate('friends')
+    // .populate('thoughts')
+    // .select('-__v');
 });
 
+//get a single user by ID
 router.get('/:userId', (req, res)=>{
     User.findOne({_id: req.params.userId}, (err, result) =>{
         if(err){
@@ -32,68 +35,92 @@ router.get('/:userId', (req, res)=>{
     .select('-__v')
 });
 
-router.post('/new', async (req, res)=>{     
+
+//create new user
+router.post('/new', async (req, res)=>{    
+    //object specs: 
     // username
-    // email
-    // 
+    // email 
     try{
-        const response = await User.create(req.body);
-        if(response){
-            res.status(200).json(response).populate('friends')
-            .populate('thoughts')
-            .select('-__v')
-           
-        }
+        if((!req.body.username && req.body.email)||(req.body.username==='')||(req.body.email==='')){
+            res.status(400).json({message: 'Please enter both a username and email.'})
+        } else { 
+            const response = await User.create(req.body)
+            if(response){
+                res.status(200).json(response)
+            }}
     } catch (err) {
-        res.status(500).json(err);
+        res.status(500).json({message:`There was an error: ${err}`});
     }
 })
 
+//edit user by id
 router.put('/edit/:id', (req, res)=>{
-    User.findOneAndUpdate(
-        //where
-        {_id: req.params.id},
-        //new value
-        {
-        username: req.body.username,
-        email: req.body.email
-        },
-        //send back new
-        {runValidators: true, new: true},
-        (err, response)=>{
-            if(err){
-                console.log(`Uhoh! There was an error: ${err}`)
-                res.status(500).json(err);
-            }
-            if(!response){
-                res.status(400).json({message: `Found no user with ID# ${req.params.id}.`})
-            }
-            if(response){
-                if((req.body.username)&&(response.thoughts.length>0)){
-                    response.thoughts.forEach(thought=>{
-                        Thought.findOneAndUpdate(
-                            //where
-                            {_id: thought._id},
-                            //new value
-                            {username: User.username},
-                            //send back new
-                            {new: true})
-                    })
-                    console.log(`User and thought(s) was updated!`)
-                    res.status(200).json(response)
-                }else{
-                    console.log(`User was updated!`)
-                    res.status(200).json(response)
+    //performs update
+    const updateUser = () => {
+        User.findOneAndUpdate(
+            //where
+            {_id: req.params.id},
+            //new value
+            {
+            username: req.body.username,
+            email: req.body.email
+            },
+            //send back new
+            {runValidators: true, new: true},
+            (err, response)=>{
+                if(err){
+                    console.log(`Uhoh! There was an error: ${err}`)
+                    res.status(500).json({message: `There was an error: ${err}`});
+                }
+                if(!response){
+                    res.status(400).json({message: `Found no user with ID# ${req.params.id}.`})
+                }
+                if(response){
+                        console.log(`User was updated!`)
+                        res.status(200).json(response)
+                    // }
                 }
             }
+        ).populate('friends')
+        .populate('thoughts')
+        .select('-__v');
+    }
+    //checks for an email, and whether or not it's formatted correctly (to avoid crashing server with validation error)
+    const checkEmail = () => {
+        if(req.body.email===''){
+            res.status(400).json({message: 'The email you entered does not appear to be formatted correctly. If you intended to update your email, please try again. Otherwise please clear this field.'})
+        } else{
+            if(req.body.email){
+                if(!/^([a-zA-Z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(req.body.email)){
+                    res.status(400).json({message: 'The email you entered does not appear to be formatted correctly. Please try again.'})
+                } else {
+                    updateUser();
+                }
+            } else {
+                updateUser();
+            }
         }
-    ).populate('friends')
-    .populate('thoughts')
-    .select('-__v');
+    } 
+    //checks for a username, and if it's already being used by someone else (to avoid crashing server with validation error)
+    if(req.body.username){
+        console.log('req.username', req.body.username)
+        if(req.body.username===' '){
+            res.status(400).json({message: 'Your username cannot be blank. Try again.'})
+        } else {
+            User.findOne({username: req.body.username}, (err, result) => {
+                if(err){throw err}
+                if(result){
+                    console.log('user', result)
+                    res.status(400).json({message: 'That username already exists. Try again.'})
+                } else {checkEmail();}
+            })
+        }
+    } else {checkEmail();}
 })
 
-
-router.put('/:id/friends/:friendId', (req, res)=>{
+//adds a friend to an existing user's friend array
+router.post('/:id/friends/:friendId', (req, res)=>{
     User.findOneAndUpdate(
         //where
         {_id: req.params.id},
@@ -112,12 +139,13 @@ router.put('/:id/friends/:friendId', (req, res)=>{
                
             }
         }
-    ) .populate('friends')
+    ) 
+    .populate('friends')
     .populate('thoughts')
     .select('-__v');
 })
 
-
+//deletes a user by id
 router.delete('/delete/:id', (req, res)=>{
     User.findOneAndDelete({_id: req.params.id}, (err, result)=>{
         if(err){
@@ -136,6 +164,32 @@ router.delete('/delete/:id', (req, res)=>{
             res.status(404).json({message:`No user with id#${req.params.id} found!`})
         }
     })
+})
+
+//deletes friend from existing user's friend array
+router.delete('/:id/friends/:friendId', (req, res)=>{
+    User.findOneAndUpdate(
+        //where
+        {_id: req.params.id},
+        //take an ID value (friendId) from set
+        {$pull: {friends: req.params.friendId}},
+        //send back new
+        {new: true},
+        (err, response)=>{
+            if(err){
+                console.log(`Uhoh! There was an error: ${err}`)
+                res.status(500).json({message: `There was an error: ${err}`});
+            } else if(!response){
+                res.status(404).json({message: 'One of the IDs is not correct. Check them and try again!'});
+            } else if (response){
+                console.log(`User was updated!`)
+                res.status(200).json(response)
+            }
+        }
+    ) 
+    .populate('friends')
+    .populate('thoughts')
+    .select('-__v');
 })
 
 module.exports = router;
